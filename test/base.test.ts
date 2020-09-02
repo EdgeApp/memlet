22ba90395e7e8a7bcd7b2133967b742522acc5d3
Sam Holmes <sam.lb.holmes@gmail.com>
2020-09-01 19:06:48 -0700

    Update base test for more clarity

--- test/base.test.ts
+++ test/base.test.ts
@@ -5,44 +5,66 @@ import { describe, it } from 'mocha'
 import { makeMemlet, Memlet } from '../src/index'
 
 export async function createObjects(memlet: Memlet) {
-  const file = { content: 'file content' }
-  const folder = { content: 'folder content' }
-  const subfolder = { content: 'subfolder content' }
+  const fileA = { content: 'file content' }
+  const folderA = { content: 'folder content' }
+  const fileB = { content: 'subfolder content' }
 
-  memlet.setJSON('file', file)
-  memlet.setJSON('folder', folder)
-  memlet.setJSON('folder/sub', subfolder)
+  memlet.setJSON('File-A', fileA)
+  memlet.setJSON('Folder-A', folderA)
+  memlet.setJSON('Folder-A/File-B', fileB)
 
-  return { file, folder, subfolder }
+  return { fileA, folderA, fileB }
 }
 
 describe('memlet', async () => {
   const disklet = makeMemoryDisklet()
   const memlet = makeMemlet(disklet)
 
-  const { file, folder, subfolder } = await createObjects(memlet)
+  const { fileA, folderA, fileB } = await createObjects(memlet)
 
-  it('can list top items', async () => {
-    expect(await memlet.list()).deep.equals({
-      file: 'file',
-      folder: 'folder'
-    })
+  it('can list root files', async () => {
+    const expected = {
+      'File-A': 'file',
+      'Folder-A': 'folder'
+    }
+
+    expect(await memlet.list()).deep.equals(expected)
   })
 
-  it('can list sub items', async () => {
-    expect(await memlet.list('folder')).deep.equals({
-      folder: 'file',
-      'folder/sub': 'file'
-    })
+  it('can list file', async () => {
+    const expected = {
+      'File-A': 'file'
+    }
 
-    expect(await memlet.list('folder/sub')).deep.equals({
-      'folder/sub': 'file'
-    })
+    expect(await memlet.list('File-A')).deep.equals(expected)
   })
 
-  it('can retrieve item', async () => {
-    expect(await memlet.getJSON('file')).deep.equals(file)
-    expect(await memlet.getJSON('folder')).deep.equals(folder)
-    expect(await memlet.getJSON('folder/sub')).deep.equals(subfolder)
+  it('can list folders', async () => {
+    const expected = {
+      'Folder-A': 'file',
+      'Folder-A/File-B': 'file'
+    }
+
+    expect(await memlet.list('Folder-A')).deep.equals(expected)
+  })
+
+  it('can list file in folder', async () => {
+    const expected = {
+      'Folder-A/File-B': 'file'
+    }
+
+    expect(await memlet.list('Folder-A/File-B')).deep.equals(expected)
+  })
+
+  it('can retrieve file', async () => {
+    expect(await memlet.getJSON('File-A')).deep.equals(fileA)
+  })
+
+  it('can retrieve folder', async () => {
+    expect(await memlet.getJSON('Folder-A')).deep.equals(folderA)
+  })
+
+  it('can retrieve file in folder', async () => {
+    expect(await memlet.getJSON('Folder-A/File-B')).deep.equals(fileB)
   })
 })
