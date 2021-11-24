da0a911d4ed4e35a7b3c71d70357b2e2fa736673
Samuel Holmes <sam@edge.app>
2021-11-23 21:32:35 -0800

    Add `memlet.list` isolation test

--- test/integrity.test.ts
+++ test/integrity.test.ts
@@ -18,3 +18,22 @@ describe('Error optimizations', () => {
     expect(await memlet.list()).deep.equals({})
   })
 })
+
+describe('Memlet isolation', () => {
+  it('wont list files from another memlet', async () => {
+    const disklet = makeMemoryDisklet()
+    const memlet1 = makeMemlet(disklet)
+    const memlet2 = makeMemlet(disklet)
+
+    await memlet1.setJson('asldjf', 'file')
+    await memlet2.setJson('bsldjf', 'file')
+
+    const [memlet1List, memlet2List] = await Promise.all([
+      memlet1.list(),
+      memlet2.list()
+    ])
+
+    expect(memlet1List).deep.equals({ asldjf: 'file' })
+    expect(memlet2List).deep.equals({ bsldjf: 'file' })
+  })
+})
